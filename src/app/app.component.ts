2b37eb5ff1d0d2e338a4d327ed47c8e68397c190
Dan <45860798+SouffleSource@users.noreply.github.com>
2025-03-01 15:46:24 +0000

    Revert "11"
    This reverts commit f70c0bad9cce446005bfac5f123d121d9f0127d1.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
@@ -13,33 +13,6 @@ Amplify.configure(outputs);
   styleUrl: './app.component.css',
   imports: [RouterOutlet, TodosComponent],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'amplify-angular-template';
-
-  ngOnInit(): void {
-    this.launchConfetti();
-  }
-
-  launchConfetti() {
-    const duration = 15 * 1000;
-    const animationEnd = Date.now() + duration;
-    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-    function randomInRange(min: number, max: number) {
-      return Math.random() * (max - min) + min;
-    }
-
-    const interval = setInterval(function() {
-      const timeLeft = animationEnd - Date.now();
-
-      if (timeLeft <= 0) {
-        return clearInterval(interval);
-      }
-
-      const particleCount = 50 * (timeLeft / duration);
-      // since particles fall down, start a bit higher than random
-      (window as any).confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
-      (window as any).confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
-    }, 250);
-  }
 }
