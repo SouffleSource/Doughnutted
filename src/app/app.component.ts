957ccc6e23f6b9805744a3c9850c63e3f7de8ee6
Dan <45860798+SouffleSource@users.noreply.github.com>
2025-03-03 19:36:45 +0000

    Revert "51"
    This reverts commit 91691e2edfeb2891da6fe92d05a4fd7c036db287.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
@@ -15,21 +15,4 @@ Amplify.configure(outputs);
 })
 export class AppComponent {
   title = 'amplify-angular-template';
-
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll() {
-    const sections = document.querySelectorAll('.section, .table-container');
-    const scrollPosition = window.scrollY + window.innerHeight / 2;
-
-    sections.forEach((section, index) => {
-      const sectionTop = section.getBoundingClientRect().top + window.scrollY;
-      const sectionBottom = sectionTop + section.clientHeight;
-
-      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-        section.classList.remove('hidden');
-      } else {
-        section.classList.add('hidden');
-      }
-    });
-  }
 }
